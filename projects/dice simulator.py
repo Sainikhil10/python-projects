abae684bc06396728b71e7d3b9c082107771535b
Sainikhil10 <70701954+Sainikhil10@users.noreply.github.com>
2022-06-16 19:04:47 +0530

    Add files via upload

---
+++ projects/dice simulator.py
@@ -0,0 +1,39 @@
+import random
+x=0
+while x==0:
+    t=input("1.press R to roll\n2. E to exit")
+    if t=="R" or t=="r":
+        z=random.randint(1,6)
+        if z==1:
+
+            print("|-----|")
+            print("|--O--|")
+            print("|-----|")
+        if z==2:
+            print("|-----|")
+            print("|O---O|")
+            print("|-----|")
+        if z==3:
+            print("|--O--|")
+            print("|-----|")
+            print("|O---O|")
+            p
+        if z==4:
+            print("|O---O|")
+            print("|-----|")
+            print("|O---O|")
+            
+        if z==5:
+            print("|O---O|")
+            print("|--O--|")
+            print("|O---O|")
+            
+        if z==6:
+            print("|O-O-O|")
+            print("|O-O-O|")
+            print("|O-O-O|")
+            
+            
+    else:
+        break
+
